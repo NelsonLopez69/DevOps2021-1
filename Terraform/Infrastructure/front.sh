fce492ad31e83b75bf03574f7758e175a09b96b8
NelsonLopez69 <nelsonstyven64gmail.com>
2021-05-02 22:00:15 -0500

    Debug del playbook de backend y base de datos

--- Terraform/Infrastructure/front.sh
+++ Terraform/Infrastructure/front.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+sudo su
 apt update
 apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
+
+
+##falta aqui la carpeteada
+##Ver el video para darse cuenta como manuel hace eso,de poner una variable de ambiente cuando ejecuta este script
+##probar el playbook del back desde cero, borrando el contenedor actual
+##Para las instancias solas si se les puede agregar una ip, con eso basta, hacer una var y meter ahi la ip de la
+##instancia de la db.
+##Buscar como obtener la ip del lb
+
+##En las paginas esta resuelto lo de el lb para back y front
+
+##Primero que todo probar el palybook del back bien, luego el front, luego el lb
+sudo ansible-playbook -i ./inventory/hosts -l back  site.yml -vvvvv  -e database_url=http://localhost:5984
