1908c7c7120df743753fb38c76982a0c23103e27
NelsonLopez69 <nelsonstyven64gmail.com>
2021-05-15 11:06:25 -0500

    Fix bugs in ansible playbooks

--- Terraform/Infrastructure/front.sh
+++ Terraform/Infrastructure/front.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-sudo su
-apt update
-apt install software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install -y ansible
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
 
 git clone https://github.com/NelsonLopez69/DevOps2021-1.git
-cd / 
 cd DevOps2021-1/Terraform/Ansible/
-ansible-playbook -i ./inventory/hosts -l ui  frontend.yml -vvvvv  -e back_host=${back_host}
+sudo ansible-playbook -i ./inventory/hosts -l ui  site.yml -vvvvv  -e back_host=${back_host} 
