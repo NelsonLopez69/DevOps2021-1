895f1eaec0d920a601933666816c9e2afd402c67
SChasqui <santiagochasqui11@gmail.com>
2021-05-21 16:24:51 -0500

    Ansible for jenkins

--- Terraform/EC2withJenkins/ec2_jenkins.tf
+++ Terraform/EC2withJenkins/ec2_jenkins.tf
@@ -9,23 +9,8 @@ resource "aws_instance" "G4_DevOps_jenkins" {
   # key name
   key_name = var.key_name
 
-  user_data = <<EOF
-		#! /bin/bash
-    sudo yum update -y
-		sudo yum install -y httpd.x86_64
-		sudo service httpd start
-		sudo service httpd enable
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+  user_data = base64encode(templatefile("./jenkins.sh"))
 
-    yum install java-1.8.0-openjdk-devel -y
-    curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
-    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-    yum install -y jenkins
-    systemctl start jenkins
-    systemctl status jenkins
-    systemctl enable jenkins
-
-	   EOF
 
   tags = {
     Name = "JenkinsEC2_G4",
---
+++ Terraform/EC2withJenkins/jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update
+apt install software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install -y ansible
+
+git clone https://github.com/NelsonLopez69/DevOps2021-1.git
+
+cd Terraform/Ansible
+
+ansible-playbook -i ./inventory/hosts -l jk site.yml -vvv
