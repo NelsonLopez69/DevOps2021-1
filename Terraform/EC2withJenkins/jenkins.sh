08102626f3e776e578f426dc3382297f8c7f73a7
SChasqui <santiagochasqui11@gmail.com>
2021-05-21 19:08:27 -0500

    Working finally

--- Terraform/EC2withJenkins/jenkins.sh
+++ Terraform/EC2withJenkins/jenkins.sh
@@ -5,9 +5,8 @@ apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
 
-git clone https://github.com/NelsonLopez69/DevOps2021-1.git
+git clone https://github.com/NelsonLopez69/DevOps2021-1.git --branch chasqui
 cd DevOps2021-1/
-git checkout --track origin/chasqui
 
 cd Terraform/Ansible
 
