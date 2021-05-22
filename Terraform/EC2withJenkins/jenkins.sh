e282e7fc85b9114ec9d99db69fcfdcecc20c7d30
SChasqui <santiagochasqui11@gmail.com>
2021-05-21 19:16:39 -0500

    Finally?

--- Terraform/EC2withJenkins/jenkins.sh
+++ Terraform/EC2withJenkins/jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-apt update
-apt install software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install -y ansible
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
 
 git clone https://github.com/NelsonLopez69/DevOps2021-1.git --branch chasqui
 cd DevOps2021-1/
 
 cd Terraform/Ansible
 
-ansible-playbook -i ./inventory/hosts -l jk site.yml -vvv
+sudo ansible-playbook -i ./inventory/hosts -l jk site.yml -vvv
